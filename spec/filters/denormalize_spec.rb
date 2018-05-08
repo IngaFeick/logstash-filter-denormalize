4f2e5d320fbb5566e9a70bbdfbfa574f9db0bab0
Inga Feick <inga.feick@trivago.com>
2018-05-08 18:09:14 +0200

    Fix test cases

--- spec/filters/denormalize_spec.rb
+++ spec/filters/denormalize_spec.rb
@@ -89,7 +89,27 @@ describe LogStash::Filters::Denormalize do
     end
   end
 
-=begin
+  describe "dictionary with target key" do
+    config <<-CONFIG
+      filter {
+        denormalize {
+          source => "my_dict"
+          target => "new_field"
+        }
+      }
+    CONFIG
+
+    sample("my_dict" => {"foo" => "bar", "cheese" => "bacon"}, "some_other_value" => "unchanged") do
+      insist { subject.length } == 2
+      subject.each do |s|
+         insist { s.get("some_other_value") } == "unchanged"
+      end
+      insist { subject[0].get("new_field") } == "bar"
+      insist { subject[1].get("new_field") } == "bacon"
+    end
+  end
+
+
 
 
 
@@ -101,21 +121,6 @@ describe LogStash::Filters::Denormalize do
     end
   end
 
- describe "array input" do
-    config <<-CONFIG
-      filter {
-        denormalize { "source" => "message"}
-      }
-    CONFIG
-
-    sample ["cheese", "bacon"] do
-      puts subject.inspect # TODO
-      insist { subject.length } == 2
-      insist { subject[0]["message"] } == "cheese"
-      insist { subject[1]["message"] } == "bacon"
-    end
-  end
-
   context "when field is not iterable" do
     it "should not raise exception" do
       filter = LogStash::Filters::Denormalize.new({"source" => "my_array"})
@@ -123,58 +128,5 @@ describe LogStash::Filters::Denormalize do
       expect {filter.filter(event)}.not_to raise_error
     end
   end
-
-
-
-  describe "change target name" do
-    config <<-CONFIG
-      filter {
-        denormalize { 
-          "source" => "message"
-          "target" => "new_key"
-        }
-      }
-    CONFIG
-
-    sample ["cheese", "bacon"] do
-      puts subject.inspect # TODO
-      insist { subject.length } == 2
-      insist { subject[0]["new_key"] } == "cheese"
-      insist { subject[1]["new_key"] } == "bacon"
-    end
-  end
-
-
-  describe "hash input" do
-    config <<-CONFIG
-      filter {
-        denormalize { "source" => "message"}
-      }
-    CONFIG
-
-    sample {"foo" => "bar", "unicorn" => "magic"} do
-      insist { subject.length } == 2
-      insist { subject[0]["foo"] }      == "bar"
-      insist { subject[1]["unicorn"] }  == "magic"
-    end
-  end
-
-  describe "hash input with target name" do
-    config <<-CONFIG
-      filter {
-        denormalize { 
-          "source" => "message"
-          "target" => "new_key"
-        }
-      }
-    CONFIG
-
-    sample {"foo" => "bar", "unicorn" => "magic"} do
-      insist { subject.length } == 2
-      insist { subject[0]["new_key"] }      == "bar"
-      insist { subject[1]["new_key"] }  == "magic"
-    end
-  end
-=end
 
 end
